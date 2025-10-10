07d152218fdd6809704527416bb24ed46a5447b4
Vansh Agrawal <vansh070605@gmail.com>
2025-10-10 23:47:13 +0530

    Added vercle

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/'  // Correct base for username.github.io repository
+  base: './'  // Use relative paths for GitHub Pages
 })
