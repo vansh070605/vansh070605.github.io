8bc531886f54f219f707a756917a2d85e8b18d54
Vansh Agrawal <vansh070605@gmail.com>
2025-10-11 05:43:20 +0530

    Configured for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: './', // relative paths for GitHub Pages
   plugins: [react()],
+  base: '/', // Correct base path for Vercel
 });
